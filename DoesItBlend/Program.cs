89f14b310e203ebe281ad0c5208cfce181ec5db1
Brenden Levi <brendenlevi@ksu.edu>
2019-09-20 16:09:05 -0500

    Final Commit

--- DoesItBlend/Program.cs
+++ DoesItBlend/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace DoesItBlend
 {
@@ -6,7 +7,21 @@ namespace DoesItBlend
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<IBlendable> items = new List<IBlendable>();
+            items.Add(new Strawberry());
+            items.Add(new Banana());
+            items.Add(new Mango());
+            items.Add(new Tomato());
+
+            items.Add(new IceCubes());
+            items.Add(new CellPhone());
+
+            string result = "";
+            foreach(IBlendable item in items)
+            {
+                result += item.Blend();
+            }
+            Console.WriteLine(result);
         }
     }
 }
